3e8fc034b192a8edb1eb4fcc10c1613b30ae9c46
Ink <eugeny.yushkevich@gmail.com>
2018-04-03 00:40:01 +0300

    little sample animation with interpolator

---
+++ app/src/main/java/freelpy/freelpy/com/heartanimation/MainActivity.kt
@@ -0,0 +1,20 @@
+package freelpy.freelpy.com.heartanimation
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.animation.AnimationUtils
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+
+		val animation = AnimationUtils.loadAnimation(this, R.anim.translatealpha)
+		animation.interpolator = MyInterpolator()
+
+		imageViewW.startAnimation(animation)
+	}
+}
---
+++ app/src/main/java/freelpy/freelpy/com/heartanimation/MyInterpolator.kt
@@ -0,0 +1,21 @@
+package freelpy.freelpy.com.heartanimation
+
+import android.view.animation.BounceInterpolator
+
+class MyInterpolator : BounceInterpolator() {
+
+	override fun getInterpolation(t: Float): Float {
+		var t1 = t
+		t1 *= 1.1226f
+		return if (t1 < 0.7408f)
+			bounce(t)
+		else if (t < 0.8644f)
+			bounce(t1 - 0.8526f) + 0.4f
+		else
+			bounce(t1 - 1.0435f) + 0.3f
+	}
+
+	private fun bounce(t: Float): Float {
+		return t * t
+	}
+}
