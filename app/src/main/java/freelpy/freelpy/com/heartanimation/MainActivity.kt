f9ad5f5dc82baac136369a2706a8c0da2f81c0f8
Eugeny Yu <eugeny.yushkevich@gmail.com>
2018-04-07 19:24:36 +0300

    fabric added

--- app/src/main/java/freelpy/freelpy/com/heartanimation/MainActivity.kt
+++ app/src/main/java/freelpy/freelpy/com/heartanimation/MainActivity.kt
@@ -4,6 +4,10 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.animation.AnimationUtils
 import kotlinx.android.synthetic.main.activity_main.*
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +18,8 @@ class MainActivity : AppCompatActivity() {
 
 		val animation = AnimationUtils.loadAnimation(this, R.anim.translatealpha)
 		animation.interpolator = MyInterpolator()
+
+		Fabric.with(this, Crashlytics())
 
 		imageViewW.startAnimation(animation)
 		//changes
